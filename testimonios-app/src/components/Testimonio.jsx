0dce125ad68d5df8f2e664feca4805ba146fb216
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-05-08 21:56:09 -0500

    componente testimonios

---
+++ testimonios-app/src/components/Testimonio.jsx
@@ -0,0 +1,16 @@
+export function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        alt="audra"
+        className="imagen-testimonio"
+        src="/imagenes/audra.webp"
+      ></img>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">Audra miller</p>
+        <p className="cargp-testimonio">Cantante en F211</p>
+        <p className="texto-testimonio">Cantante principal del grupo F211</p>
+      </div>
+    </div>
+  );
+}

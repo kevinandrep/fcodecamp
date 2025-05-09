fea2cda74c00a20653db220286a41f44db2d9304
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-05-09 09:55:13 -0500

    props

--- testimonios-app/src/App.jsx
+++ testimonios-app/src/App.jsx
@@ -1,12 +1,50 @@
 import "./App.css";
 import { Testimonio } from "./components/Testimonio.jsx";
 
+const singers = [
+  {
+    name: "Audra",
+    cargo: "vocalista",
+    texto: "Cantante principal de F211",
+    imagen: "/imagenes/audra.webp",
+  },
+  {
+    name: "Yuqi",
+    cargo: "Voz de grupo",
+    texto: "Cantante del grupo Gidle",
+    imagen: "/imagenes/yuqi.jpg",
+  },
+  {
+    name: "Soyeon",
+    cargo: "Lieadr",
+    texto: "Lider y productor principal de Gidle",
+    imagen: "/imagenes/soyeon.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <div className="contenedor-principal">
         <h1>Principal singers</h1>
-        <Testimonio></Testimonio>
+        <Testimonio
+          nombre={singers[0].name}
+          cargo={singers[0].cargo}
+          texto={singers[0].texto}
+          imagen={singers[0].imagen}
+        ></Testimonio>
+        <Testimonio
+          nombre={singers[1].name}
+          cargo={singers[1].cargo}
+          texto={singers[1].texto}
+          imagen={singers[1].imagen}
+        ></Testimonio>
+        <Testimonio
+          nombre={singers[2].name}
+          cargo={singers[2].cargo}
+          texto={singers[2].texto}
+          imagen={singers[2].imagen}
+        ></Testimonio>
       </div>
     </div>
   );
--- testimonios-app/src/components/Testimonio.jsx
+++ testimonios-app/src/components/Testimonio.jsx
@@ -1,17 +1,13 @@
 import "../styles/Testimonios.css";
 
-export function Testimonio() {
+export function Testimonio({ nombre, cargo, texto, imagen }) {
   return (
     <div className="contenedor-testimonio">
-      <img
-        alt="audra"
-        className="imagen-testimonio"
-        src="/imagenes/audra.webp"
-      ></img>
+      <img alt="audra" className="imagen-testimonio" src={imagen}></img>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Audra miller</p>
-        <p className="cargp-testimonio">Cantante en F211</p>
-        <p className="texto-testimonio">Cantante principal del grupo F211</p>
+        <p className="nombre-testimonio">{nombre}</p>
+        <p className="cargo-testimonio">{cargo}</p>
+        <p className="texto-testimonio">{texto}</p>
       </div>
     </div>
   );

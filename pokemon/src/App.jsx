0ffa54996146f39c5134f497112c14595d932cd1
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-05-22 17:32:43 -0500

    CAMBIOS

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -3,36 +3,32 @@ import "./App.css";
 
 function App() {
   const [pokemon, setPokemon] = useState(null);
-  const [input, setInput] = useState("");
+  const [index, setIndex] = useState(1);
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/9`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
       .then((res) => res.json())
       .then((data) => setPokemon(data));
-  }, []);
+  }, [index]);
 
   if (!pokemon) {
     return <p>Cargando...</p>;
   }
   console.log(pokemon);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
-      .then((res) => res.json())
-      .then((data) => setPokemon(data));
+  const handleAnterior = () => {
+    if (index <= 1) return;
+    setIndex((prev) => prev - 1);
   };
 
-  const handleChange = (e) => {
-    setInput(e.target.value);
+  const handleSiguiente = () => {
+    setIndex((prev) => prev + 1);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input value={input} onChange={handleChange}></input>
-        <button>buscar</button>
-      </form>
+      <button onClick={handleAnterior}>Anterior</button>
+      <button onClick={handleSiguiente}>Siguiente</button>
       <p>{pokemon.name}</p>
       <img src={pokemon.sprites.front_default}></img>
     </div>

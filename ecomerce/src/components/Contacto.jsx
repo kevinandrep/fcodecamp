f535179e61733fe8bca892d8fe93e7a8d8003dbe
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-05-27 17:45:31 -0500

    formularios con npm react-hook-form

--- ecomerce/src/components/Contacto.jsx
+++ ecomerce/src/components/Contacto.jsx
@@ -1,37 +1,17 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export function Contacto() {
-  const [valores, setValores] = useState({
-    nombre: "",
-    email: "",
-  });
-
-  const handleValores = (event) => {
-    setValores({ ...valores, [event.target.name]: event.target.value });
+  const { register, handleSubmit } = useForm();
+  const enviar = (data) => {
+    console.log(data);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(valores);
-  };
   return (
     <div className="container">
       <h1 className="main-title">Contacto</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={valores.nombre}
-          onChange={handleValores}
-          type="text"
-          placeholder="Nombre"
-          name="nombre"
-        ></input>
-        <input
-          value={valores.email}
-          onChange={handleValores}
-          type="email"
-          placeholder="Correo"
-          name="email"
-        ></input>
+      <form className="formulario" onSubmit={handleSubmit(enviar)}>
+        <input {...register("nombre")} type="text" placeholder="Nombre"></input>
+        <input {...register("email")} type="email" placeholder="Correo"></input>
         <button className="enviar" type="submit">
           Enviar
         </button>

aeeb76dcb5763a4887e8c3c167d9f5f70629b56f
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-01 15:59:56 -0500

    gradients y backgraound fixed

--- landinpage-test/src/App.jsx
+++ landinpage-test/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Banner } from "./assets/components/Banner";
+import { Card } from "./assets/components/Card";
+import { Fondos } from "./assets/components/Fondos";
 import { NavBar } from "./assets/components/NavBar";
 
 function App() {
   return (
     <>
-      <NavBar></NavBar>
-      <Banner></Banner>
+      {/* <NavBar></NavBar>
+      <Banner></Banner> */}
+      <Fondos></Fondos>
     </>
   );
 }

94229a8d8046974c7d5fefe86c99bcfefd824eb0
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-01 22:46:59 -0500

    sombras

--- landinpage-test/src/App.jsx
+++ landinpage-test/src/App.jsx
@@ -3,13 +3,14 @@ import { Banner } from "./assets/components/Banner";
 import { Card } from "./assets/components/Card";
 import { Fondos } from "./assets/components/Fondos";
 import { NavBar } from "./assets/components/NavBar";
+import { Sombras } from "./assets/components/Sombras";
 
 function App() {
   return (
     <>
       {/* <NavBar></NavBar>
       <Banner></Banner> */}
-      <Fondos></Fondos>
+      <Sombras></Sombras>
     </>
   );
 }
---
+++ landinpage-test/src/assets/components/Sombras.jsx
@@ -0,0 +1,19 @@
+import "../estilos/sombras.css";
+export function Sombras() {
+  return (
+    <div className="sombras">
+      <div className="sombras-caja">
+        <div className="contenedor-imagen">
+          <img src="./images/frieren.webp" alt="frieren" />
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nisi
+          facere ab repudiandae deserunt impedit distinctio incidunt obcaecati
+          architecto exercitationem delectus aut, maxime sed minus! Doloremque
+          dolorum sapiente animi fugiat?
+        </p>
+      </div>
+    </div>
+  );
+}

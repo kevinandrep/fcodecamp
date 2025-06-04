c46040965eeef5ac08add98146b89e083c488a56
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-04 15:01:12 -0500

    positions fixed sticky

--- landinpage-test/src/App.jsx
+++ landinpage-test/src/App.jsx
@@ -1,18 +1,10 @@
 import "./App.css";
-import { Banner } from "./assets/components/Banner";
-import { Card } from "./assets/components/Card";
-import { Fondos } from "./assets/components/Fondos";
-import { Modal } from "./assets/components/Modal";
-import { NavBar } from "./assets/components/NavBar";
-import { Position } from "./assets/components/Position";
-import { Sombras } from "./assets/components/Sombras";
+import { Positionfs } from "./assets/components/Positionfs";
 
 function App() {
   return (
     <>
-      {/* <NavBar></NavBar>
-      <Banner></Banner> */}
-      <Modal></Modal>
+      <Positionfs></Positionfs>
     </>
   );
 }
---
+++ landinpage-test/src/assets/components/Positionfs.jsx
@@ -0,0 +1,54 @@
+import "../estilos/positionfs.css";
+export function Positionfs() {
+  return (
+    <div>
+      <div className="contenedor">
+        <div className="box"></div>
+      </div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dolorem
+      nihil saepe sint, sequi nesciunt doloremque voluptatibus veniam odio, illo
+      deserunt ut et quia sit officiis necessitatibus itaque quos ullam? Lorem
+      ipsum dolor sit amet consectetur adipisicinoloremque voluptatibus veniam
+      odio, illo deserunt ut et quia sit officiis necessitatibus itaque quos
+      ullam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+      repudiandae corrupti quo odit similique, qui quisquam. Animi reiciendis
+      sit atque a, consequatur repellendus quasi quia minima. Velit totam ad
+      tempore.lo Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Assumenda, veniam obcaecati voluptatibuoloremque voluptatibus veniam odio,
+      illo deserunt ut et quia sit officiis necessitatibus itaque quos ullam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae
+      corrupti quo odit similique, qui quisquam. Animi reiciendis sit atque a,
+      consequatur repellendus quasi quia minima. Velit totam ad tempore.lo Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Assumenda, veniam
+      obcaecati voluptatibuoloremque voluptatibus veniam odio, illo deserunt ut
+      et quia sit officiis necessitatibus itaque quos ullam? Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Quas repudiandae corrupti quo odit
+      similique, qui quisquam. Animi reiciendis sit atque a, consequatur
+      repellendus quasi quia minima. Velit totam ad tempore.lo Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Assumenda, veniam obcaecati
+      voluptatibug elit. Quas repudiandae corrupti quo odit similique, qui
+      quisquam. Animi reiciendis sit atque a, consequatur repellendus quasi quia
+      minima. Velit totam ad tempore.lo Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Assumenda, veniam obcaecati voluptatibus odit
+      consequatur excepturi iste distinctio rem saepe praesentium nostrum
+      aliquid reiciendis doloremque, quasi in quia velit dignissimos voluptates.
+      lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, voluptatum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nihil
+      provident sit accusantium obcaecati labore, ad in distinctio, perspiciatis
+      velit, animi enim odio. Nesciunt dolor ex iure hic repudiandae temporibus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, ex rem?
+      Eveniet doloribus nesciunt distinctio laborum iusto quas cumque placeat
+      officia deserunt quo sequi beatae tempore, assumenda earum ipsum
+      blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Molestias, sed adipisci ut explicabo quos praesentium atque odit sit ab
+      corrupti pariatur temporibus, quidem quam error, voluptas recusandae
+      accusantium. Nostrum, quod? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Eaque laudantium facilis laboriosam ipsa blanditiis!
+      Atque quaerat necessitatibus eligendi hic eum est doloremque expedita
+      dolores. Tempore aut magnam harum consequuntur est? Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Voluptatem odit quibusdam quisquam vel
+      consectetur consequatur, ad laborum iste quos blanditiis assumenda ab,
+      vitae inventore alias obcaecati eos eaque beatae atque.
+    </div>
+  );
+}

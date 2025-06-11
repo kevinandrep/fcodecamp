55856c3698156ff30aee5b787be80fe5e1314a72
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-11 11:38:07 -0500

    flex grow y shrink

--- landinpage-test/src/assets/components/Flexbox.jsx
+++ landinpage-test/src/assets/components/Flexbox.jsx
@@ -3,12 +3,12 @@ import "../estilos/flex.css";
 export function Flexbox() {
   return (
     <div className="flexbox">
-      <div className="flex-item">1</div>
+      <div className="flex-itemg">1</div>
       <div className="flex-item">2</div>
       <div className="flex-item">3</div>
       <div className="flex-item">4</div>
       <div className="flex-item">5</div>
-      <div className="flex-item">6</div>
+      <div className="flex-itemg">6</div>
       <div className="flex-item">7</div>
     </div>
   );

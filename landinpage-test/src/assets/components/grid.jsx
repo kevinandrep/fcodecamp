e391f0876309a0d4c1dc2b5699a36f9992ac6da7
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-18 11:27:41 -0500

    gridrowsstate

--- landinpage-test/src/assets/components/grid.jsx
+++ landinpage-test/src/assets/components/grid.jsx
@@ -2,18 +2,46 @@ import "../estilos/Grid.css";
 export function Grid() {
   return (
     <div className="grid">
-      <div className="grid-item">1</div>
-      <div className="grid-item">2</div>
-      <div className="grid-item">3</div>
-      <div className="grid-item">4</div>
-      <div className="grid-item">5</div>
-      <div className="grid-item">6</div>
-      <div className="grid-item">7</div>
-      <div className="grid-item">8</div>
-      <div className="grid-item">9</div>
-      <div className="grid-item">7</div>
-      <div className="grid-item">8</div>
-      <div className="grid-item">9</div>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
+      <img
+        className="grid-item"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
+      ></img>
     </div>
   );
 }

ace5e812125e06b1a24ca259673f9f3c1b4ebffc
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-18 10:47:40 -0500

    gap

--- landinpage-test/src/assets/components/grid.jsx
+++ landinpage-test/src/assets/components/grid.jsx
@@ -8,6 +8,12 @@ export function Grid() {
       <div className="grid-item">4</div>
       <div className="grid-item">5</div>
       <div className="grid-item">6</div>
+      <div className="grid-item">7</div>
+      <div className="grid-item">8</div>
+      <div className="grid-item">9</div>
+      <div className="grid-item">7</div>
+      <div className="grid-item">8</div>
+      <div className="grid-item">9</div>
     </div>
   );
 }

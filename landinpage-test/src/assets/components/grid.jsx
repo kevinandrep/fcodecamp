fbd473f3a87f907119c41cd4fb28a9f961037c58
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-19 10:58:26 -0500

    gridrowstartend

--- landinpage-test/src/assets/components/grid.jsx
+++ landinpage-test/src/assets/components/grid.jsx
@@ -2,46 +2,22 @@ import "../estilos/Grid.css";
 export function Grid() {
   return (
     <div className="grid">
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
-      <img
-        className="grid-item"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS61M_58xZfzIDZjaaqHiUNazL0Su3LKxraGw&s"
-      ></img>
+      <header>HEADER</header>
+      <nav>NAV</nav>
+      <main>
+        <h1>TITULO PRINCIPAL</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          perferendis corrupti itaque vero. Velit eum cumque corrupti adipisci,
+          sequi, recusandae aliquam quia neque explicabo aliquid magnam! Error
+          est dicta ipsum? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Accusantium possimus optio sapiente fuga maiores eligendi qui
+          cumque, quidem temporibus assumenda corrupti soluta ab minus.
+          Accusantium atque obcaecati officia similique sequi.
+        </p>
+      </main>
+      <aside>ASIDE</aside>
+      <footer>FOOTER</footer>
     </div>
   );
 }

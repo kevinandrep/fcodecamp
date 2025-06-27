18e3f40368d69b90dbec327b76ce7597000c6f39
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-06-27 11:44:57 -0500

    logics js y localstorage

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -5,10 +5,19 @@ import { checkWinner } from "./logic/board.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
 import "./App.css";
 import { Board } from "./components/Board.jsx";
+import { resetGameStorage, saveGame } from "./logic/storage/index.js";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromLocalStorage = window.localStorage.getItem("board");
+    if (boardFromLocalStorage) return JSON.parse(boardFromLocalStorage);
+    return Array(9).fill(null);
+  });
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
 
   const checkGameOver = (newBoard) => {
@@ -20,7 +29,15 @@ function App() {
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
-    setTurn(turn === TURNS.X ? TURNS.O : TURNS.X);
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    saveGame({
+      board: newBoard,
+      newTurn: newTurn,
+    });
+
+    //guardar qui partida
+
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       setWinner(newWinner);
@@ -33,10 +50,13 @@ function App() {
     setBoard(Array(9).fill(null));
     setWinner(null);
     setTurn(TURNS.X);
+
+    resetGameStorage();
   };
   return (
     <>
       <main className="board">
+        <button onClick={resetGame}>Reset</button>
         <section className="game">
           <Board board={board} updateBoard={updateBoard}></Board>
         </section>

dba85d92b54030884eb6dcc9096cdcb335955a87
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-07-11 17:09:49 -0500

    query no controlada

--- prueba-tecnica/prueba-tecnica-movies/src/App.jsx
+++ prueba-tecnica/prueba-tecnica-movies/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "./App.css";
 
 import { Movies } from "./components/movies";
@@ -6,12 +7,19 @@ import { useMovies } from "./hooks/useMovies";
 function App() {
   const { movies } = useMovies();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const fields = new FormData(e.target);
+    const query = fields.get("query");
+    console.log(query);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className="form">
-          <input type="text" name="search" id="search" placeholder="Search" />
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" name="query" id="search" placeholder="Search" />
           <button type="input">Buscar</button>
         </form>
       </header>

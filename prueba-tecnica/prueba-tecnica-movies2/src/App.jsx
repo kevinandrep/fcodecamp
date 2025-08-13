1e87d2b0b6709d0e134e10e0ab9664ee0ef80ac6
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-08-13 15:38:35 -0500

    cargando desde json estatico

--- prueba-tecnica/prueba-tecnica-movies2/src/App.jsx
+++ prueba-tecnica/prueba-tecnica-movies2/src/App.jsx
@@ -1,10 +1,38 @@
 import "./App.css";
 
+import responseMovies from "./mocks/with-results.json";
+import withoutResults from "./mocks/with-no-results.json";
+
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
+
   return (
-    <>
-      <div>Movies</div>
-    </>
+    <div>
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className="form">
+          <input type="text" placeholder="Search movies"></input>
+          <button type="submit">Search</button>
+        </form>
+      </header>
+
+      <main>
+        {hasMovies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title}></img>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No se encontraron peliculas</p>
+        )}
+      </main>
+    </div>
   );
 }
 

a7937364bd88bfb5bb1c7e832416c9601d37c0b6
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-08-14 12:47:48 -0500

    componentes y custom hook usemovies

--- prueba-tecnica/prueba-tecnica-movies2/src/App.jsx
+++ prueba-tecnica/prueba-tecnica-movies2/src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
-
-import responseMovies from "./mocks/with-results.json";
-import withoutResults from "./mocks/with-no-results.json";
+import { Movies } from "./components/movies";
+import { useMovies } from "./hooks/useMovies";
 
 function App() {
-  const movies = responseMovies.Search;
-  const hasMovies = movies?.length > 0;
-
+  const { movies } = useMovies();
   return (
     <div>
       <header>
@@ -18,19 +15,7 @@ function App() {
       </header>
 
       <main>
-        {hasMovies ? (
-          <ul>
-            {movies.map((movie) => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title}></img>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No se encontraron peliculas</p>
-        )}
+        <Movies movies={movies}></Movies>
       </main>
     </div>
   );
---
+++ prueba-tecnica/prueba-tecnica-movies2/src/components/movies.jsx
@@ -0,0 +1,26 @@
+function ListOfMovies({ movies }) {
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.title}></img>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function NoMoviesResult() {
+  return <p>No se encontdraron peliculas</p>;
+}
+
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0;
+  return hasMovies ? (
+    <ListOfMovies movies={movies} />
+  ) : (
+    <NoMoviesResult></NoMoviesResult>
+  );
+}

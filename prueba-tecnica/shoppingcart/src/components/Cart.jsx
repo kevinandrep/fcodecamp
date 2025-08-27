a9d046d94909c8bea3b9862593d2679ca6667bfc
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-08-26 21:01:42 -0500

    carrito funcional con context agregar y eliminar

--- prueba-tecnica/shoppingcart/src/components/Cart.jsx
+++ prueba-tecnica/shoppingcart/src/components/Cart.jsx
@@ -1,9 +1,26 @@
 import { useId } from "react";
 import { CartIcon, ClearCartIcon } from "./icons";
 import "./Cart.css";
+import { useCart } from "../hooks/useCart";
+
+function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img src={thumbnail} alt={title}></img>
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+      <footer>
+        <small>Qty: {quantity}</small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  );
+}
 
 export function Cart() {
   const cartCheckboxId = useId();
+  const { cart, clearCart, addToCart } = useCart();
   return (
     <>
       <label className="cart-button" htmlFor={cartCheckboxId}>
@@ -13,21 +30,15 @@ export function Cart() {
 
       <aside className="cart">
         <ul>
-          <li>
-            <img
-              src="https://cdn.dummyjson.com/product-images/7/thumbnail.jpg"
-              alt="iphone"
-            ></img>
-            <div>
-              <strong>Iphone</strong> - $1499
-            </div>
-            <footer>
-              <small>Qty: 1</small>
-              <button>+</button>
-            </footer>
-          </li>
+          {cart.map((product) => (
+            <CartItem
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              {...product}
+            ></CartItem>
+          ))}
         </ul>
-        <button>
+        <button onClick={clearCart}>
           <ClearCartIcon></ClearCartIcon>
         </button>
       </aside>

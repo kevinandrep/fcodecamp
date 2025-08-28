e1970adc17126cecbe8bc10222905a3c213847f7
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-08-28 10:47:29 -0500

    crear rouer para hacerlo mas declarativo

--- prueba-tecnica/proy-roter/src/App.jsx
+++ prueba-tecnica/proy-roter/src/App.jsx
@@ -1,29 +1,23 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { EVENTS } from "./assets/consts";
+import { Router } from "./Router.jsx";
 import HomePage from "./pages/Home.jsx";
 import AboutPage from "./pages/About.jsx";
 
+const appRoutes = [
+  {
+    path: "/",
+    Component: HomePage,
+  },
+  {
+    path: "/about",
+    Component: AboutPage,
+  },
+];
+
 function App() {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
-
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname);
-    };
-
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange);
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange);
-
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange);
-      window.removeEventListener(EVENTS.POPSTATE, onLocationChange);
-    };
-  }, []);
   return (
     <main>
-      {currentPath === "/" && <HomePage></HomePage>}
-      {currentPath === "/about" && <AboutPage></AboutPage>}
+      <Router routes={appRoutes}></Router>
     </main>
   );
 }

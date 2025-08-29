bc6f7169b4410bc7f07458b5bb41cc8d9eba747c
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-08-29 14:21:45 -0500

    final ttt

--- prueba-tecnica/react-doc-3r/src/components/Board.jsx
+++ prueba-tecnica/react-doc-3r/src/components/Board.jsx
@@ -4,10 +4,12 @@ import { Square } from "./Square";
 export function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  let status;
 
   const handleClick = (pos) => {
     setSquares((prevSquares) => {
-      if (prevSquares[pos]) return prevSquares;
+      if (prevSquares[pos] || calculateWinner(prevSquares)) return prevSquares;
+
       const newSquares = prevSquares.slice();
       newSquares[pos] = xIsNext ? "X" : "O";
       return newSquares;
@@ -15,8 +17,43 @@ export function Board() {
     setXIsNext((prev) => !prev);
   };
 
+  const calculateWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (squares.every((square) => square)) {
+    status = "Draw";
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square
           onSquareClick={() => handleClick(0)}

97527215dd01145a303e0591696747f848bf343b
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-09-02 23:44:30 -0500

    to 36

---
+++ practicejs/ejercicio36.js
@@ -0,0 +1,52 @@
+// 📝 Ejercicio:
+
+// Tienes este array de empleados:
+
+const empleados = [
+  { id: 1, nombre: "Ana", salario: 2000, activo: true, departamento: "Ventas" },
+  {
+    id: 2,
+    nombre: "Luis",
+    salario: 2500,
+    activo: false,
+    departamento: "Marketing",
+  },
+  { id: 3, nombre: "Marta", salario: 3000, activo: true, departamento: "IT" },
+];
+
+// 👉 Haz lo siguiente:
+
+// Agrega un nuevo empleado:
+
+// { id: 4, nombre: "Pedro", salario: 2200, activo: true, departamento: "Ventas" }
+
+// Aumenta en 10% el salario de Marta (id=3).
+
+// Cambia el estado de Luis (id=2) a activo: true.
+
+// Elimina al empleado con id=1 (Ana).
+
+// Usa find para obtener el empleado con mayor salario y muéstralo por consola.
+
+const final = [
+  ...empleados,
+  {
+    id: 4,
+    nombre: "Pedro",
+    salario: 2200,
+    activo: true,
+    departamento: "Ventas",
+  },
+]
+  .map((empleado) => {
+    if (empleado.id === 3)
+      return { ...empleado, salario: empleado.salario * 1.1 };
+    if (empleado.id === 2) return { ...empleado, activo: true };
+    return empleado;
+  })
+  .filter((empleado) => empleado.id !== 1)
+  .reduce((max, emp) => {
+    return emp.salario > max.salario ? emp : max;
+  });
+
+console.log(final);
